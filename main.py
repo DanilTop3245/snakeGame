381d8b3dc82a377ca166cd62ece1812cab93c570
DanilTop3245 <turasov2009@gmail.com>
2024-12-07 18:09:15 +0200

    add realization button play, errors

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame, random, sys
 from pygame.math import Vector2 as vk2
-import settings, game_functions, fruit, snake, menu
+import settings, game_functions, fruit, snake
 
 
 def main():
@@ -33,4 +33,4 @@ def main():
 
 
 if __name__ == '__main__':
-    menu.show_menu()
+    main()
--- menu.py
+++ menu.py
@@ -1,8 +1,10 @@
-import sys, pygame
+import subprocess
+import sys
+import pygame
 import settings
 
-SCREEN_HEIGHT = 400
-SCREEN_WIDTH = 350
+SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 800
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 PLAY_BUTTON = pygame.Rect(SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT / 2 - 50, 200, 50)
 QUIT_BUTTON = pygame.Rect(SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT / 2 + 60, 200, 50)
@@ -10,6 +12,10 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 PLAY_BUTTON_COLOR = (14, 176, 92)
 QUIT_BUTTON_COLOR = (160, 29, 30)
+
+
+def run_game():
+    subprocess.Popen([sys.executable, "main.py"])
 
 
 def show_menu():
@@ -21,12 +27,26 @@ def show_menu():
         draw_text("Play", settings.GAME_FONT, BLACK, SCREEN, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 25)
         draw_text("Quit", settings.GAME_FONT, BLACK, SCREEN, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 85)
         pygame.display.update()
-        
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                if PLAY_BUTTON.collidepoint(mouse_pos):
+                    run_game()
+                    pygame.quit()
+                    sys.exit()
+                if QUIT_BUTTON.collidepoint(mouse_pos):
+                    pygame.quit()
+                    sys.exit()
 
 
 def draw_text(text, font, color, surface, x, y):
     text_obj = font.render(text, True, color)
     text_rect = text_obj.get_rect(center=(x, y))
     surface.blit(text_obj, text_rect)
+
 
 show_menu()
